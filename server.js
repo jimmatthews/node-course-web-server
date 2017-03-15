0849fd9d8ba9c222f2161cf205a357a38d6d78cf
Jim Matthews <matthews@fetchsoftworks.com>
2017-03-15 17:53:55 -0400

    setup for Heroku (video 48)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -55,13 +57,12 @@ app.get('/about', (req, res) => {
     });
 });
 
-
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Error handling request'
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
